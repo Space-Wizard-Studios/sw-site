efe003fa53e3c0671e5205cff11885c0aabcd27a
William <william1504@gmail.com>
2022-09-22 13:12:03 -0300

    tsx

---
+++ src/components/atoms/ToggleCard.tsx
@@ -0,0 +1,39 @@
+import { useState } from 'react';
+import type { ReactNode } from 'react';
+
+export interface ToggleCardProps {
+	cardIcon?: ReactNode;
+	title?: ReactNode;
+	subtitle?: ReactNode;
+	description?: ReactNode;
+	buttonIconMinus?: ReactNode;
+	buttonIconPlus?: ReactNode;
+}
+
+export default function ToggleCard({
+	cardIcon,
+	title,
+	subtitle,
+	description,
+	buttonIconMinus,
+	buttonIconPlus,
+}: ToggleCardProps) {
+	const [isOpen, setOpen] = useState(false);
+	return (
+		<div className="card flex-grow: 1 w-96 relative overflow-visible sw-bg sw-text-reverse">
+			<div className="absolute -mt-8 -mr-8 right-0">{cardIcon}</div>
+			<div className="card-body">
+				<div>{title}</div>
+				<div>{isOpen ? description : subtitle}</div>
+				<button
+					type="button"
+					className="rounded-lg text-sm p-2.5 inline-flex items-center relative sw-text-reverse"
+					aria-label="Toggle"
+					onClick={() => setOpen(!isOpen)}
+				>
+					{isOpen ? buttonIconMinus : buttonIconPlus}
+				</button>
+			</div>
+		</div>
+	);
+}
