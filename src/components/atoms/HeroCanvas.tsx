65121ef7c71b64513aa0202eafac8f6a54d3c550
William <william1504@gmail.com>
2022-09-22 19:10:45 -0300

    estrela, luz e os caralho

--- src/components/atoms/HeroCanvas.tsx
+++ src/components/atoms/HeroCanvas.tsx
@@ -1,6 +1,6 @@
 import { Suspense } from 'react';
 import { Canvas } from '@react-three/fiber';
-import { PerspectiveCamera } from '@react-three/drei';
+import { PerspectiveCamera, Stars, Stage, SpotLight, RandomizedLight } from '@react-three/drei';
 import HeroModel from './HeroModel';
 
 export interface HeroCanvasProps {}
@@ -9,10 +9,36 @@ export default function CardIconCanvas({}: HeroCanvasProps) {
 	return (
 		<Canvas>
 			<PerspectiveCamera makeDefault fov={60} position={[0, 0, 10]} />
-			<pointLight position={[10, 10, 10]} intensity={1.2} />
-			<Suspense fallback={null}>
-				<HeroModel />
-			</Suspense>
+			<Stars radius={100} depth={50} count={3000} factor={4} saturation={0} speed={2.3} fade />
+			<Stage
+				adjustCamera
+				ambience={1}
+				intensity={0.6}
+				environment={'night'}
+				shadows
+				contactShadow={{ blur: 0.2, opacity: 0.7 }}
+			>
+				<Suspense fallback={null}>
+					<HeroModel />
+				</Suspense>
+			</Stage>
+
+			<SpotLight
+				position={[+3.3, 5.6, +1]}
+				color={0xf22312}
+				distance={7}
+				angle={0.6}
+				attenuation={7}
+				anglePower={5}
+			/>
+			<SpotLight
+				position={[-3.3, 5.6, -1]}
+				color={0x2312f2}
+				distance={7}
+				angle={0.6}
+				attenuation={7}
+				anglePower={5}
+			/>
 		</Canvas>
 	);
 }
