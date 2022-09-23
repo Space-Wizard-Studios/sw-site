96b9bdbfffa8caec4dc68179dfe67d371ed20995
William <william1504@gmail.com>
2022-09-23 10:48:08 -0300

    Hero model

--- src/components/atoms/HeroCanvas.tsx
+++ src/components/atoms/HeroCanvas.tsx
@@ -1,44 +1,52 @@
 import { Suspense } from 'react';
 import { Canvas } from '@react-three/fiber';
-import { PerspectiveCamera, Stars, Stage, SpotLight, RandomizedLight } from '@react-three/drei';
+import { PerspectiveCamera, Stars, Stage, SpotLight } from '@react-three/drei';
 import HeroModel from './HeroModel';
 
-export interface HeroCanvasProps {}
+export interface HeroCanvasProps {
+	modelPath: string;
+}
 
-export default function CardIconCanvas({}: HeroCanvasProps) {
+export default function CardIconCanvas({ modelPath }: HeroCanvasProps) {
 	return (
 		<Canvas>
-			<PerspectiveCamera makeDefault fov={60} position={[0, 0, 10]} />
+			<PerspectiveCamera makeDefault fov={25} position={[0, 0, 10]} />
 			<Stars radius={100} depth={50} count={3000} factor={4} saturation={0} speed={2.3} fade />
-			<Stage
-				adjustCamera
-				ambience={1}
-				intensity={0.6}
-				environment={'night'}
-				shadows
-				contactShadow={{ blur: 0.2, opacity: 0.7 }}
-			>
-				<Suspense fallback={null}>
-					<HeroModel />
-				</Suspense>
-			</Stage>
 
-			<SpotLight
-				position={[+3.3, 5.6, +1]}
-				color={0xf22312}
-				distance={7}
-				angle={0.6}
-				attenuation={7}
-				anglePower={5}
-			/>
-			<SpotLight
-				position={[-3.3, 5.6, -1]}
-				color={0x2312f2}
-				distance={7}
-				angle={0.6}
-				attenuation={7}
-				anglePower={5}
-			/>
+			<Suspense fallback={null}>
+				<SpotLight
+					position={[2.4, 2.8, 0.2]}
+					color={0xb22312}
+					distance={8}
+					angle={0.6}
+					attenuation={6}
+					anglePower={7}
+				/>
+				<SpotLight
+					position={[-2.4, 2.8, -0.2]}
+					color={0x2312f2}
+					distance={8}
+					angle={0.6}
+					attenuation={6}
+					anglePower={7}
+				/>
+
+				<Stage
+					adjustCamera
+					ambience={1}
+					intensity={0.8}
+					environment={'night'}
+					shadows={false}
+					contactShadow={false}
+				>
+					<HeroModel
+						position={[0, 0, 0]}
+						rotation={[Math.PI / 8, -Math.PI / 3, Math.PI / 12]}
+						scale={1}
+						modelPath={modelPath}
+					/>
+				</Stage>
+			</Suspense>
 		</Canvas>
 	);
 }
--- src/components/atoms/HeroModel.tsx
+++ src/components/atoms/HeroModel.tsx
@@ -1,21 +1,112 @@
-import { useRef } from 'react';
-import { useFrame } from '@react-three/fiber';
+import { useRef, useEffect } from 'react';
+import { useGLTF, useAnimations } from '@react-three/drei';
+import { AnimationActionLoopStyles, LoopPingPong } from 'three';
 
-export interface HeroModelProps {}
+export interface HeroModelProps {
+	modelPath: string;
+	[props: string]: any;
+}
 
-export default function HeroModel({}: HeroModelProps) {
+export default function HeroModel({ modelPath, ...props }: HeroModelProps) {
 	const ref = useRef(null);
 
-	useFrame((state, delta) => {
-		if (ref.current === null) return;
-		ref.current.rotation.x += delta * 0.7;
-		ref.current.rotation.y += delta * 0.7;
+	const { nodes, materials, animations } = useGLTF(modelPath);
+	const { mixer, names, actions, clips } = useAnimations(animations, ref);
+
+	useEffect(() => {
+		let action = actions.char_anim.play();
+		// action.loop = LoopPingPong;
+		action.timeScale = 0.25;
 	});
 
 	return (
-		<mesh ref={ref} position={[0, 0, 0]}>
-			<torusKnotGeometry args={[1, 0.4, 100, 16]} />
-			<meshLambertMaterial color={0x4259f1} wireframe />
-		</mesh>
+		<group ref={ref} {...props} dispose={null}>
+			<group name="Scene">
+				<group name="astronaut_rigify">
+					<primitive object={nodes.root} />
+					<primitive object={nodes['MCH-torsoparent']} />
+					<primitive object={nodes['MCH-foot_ikparentL']} />
+					<primitive object={nodes['MCH-thigh_ik_targetparentL']} />
+					<primitive object={nodes['MCH-foot_ikparentR']} />
+					<primitive object={nodes['MCH-thigh_ik_targetparentR']} />
+					<primitive object={nodes['MCH-hand_ikparentL']} />
+					<primitive object={nodes['MCH-upper_arm_ik_targetparentL']} />
+					<primitive object={nodes['MCH-hand_ikparentR']} />
+					<primitive object={nodes['MCH-upper_arm_ik_targetparentR']} />
+					<group name="character">
+						<skinnedMesh
+							name="Cube019"
+							geometry={nodes.Cube019.geometry}
+							material={nodes.Cube019.material}
+							skeleton={nodes.Cube019.skeleton}
+							material-color="red"
+						/>
+						<skinnedMesh
+							name="Cube019_1"
+							geometry={nodes.Cube019_1.geometry}
+							material={nodes.Cube019_1.material}
+							skeleton={nodes.Cube019_1.skeleton}
+							material-color="green"
+						/>
+						<skinnedMesh
+							name="Cube019_2"
+							geometry={nodes.Cube019_2.geometry}
+							material={nodes.Cube019_2.material}
+							skeleton={nodes.Cube019_2.skeleton}
+							material-color="blue"
+						/>
+						<skinnedMesh
+							name="Cube019_3"
+							geometry={nodes.Cube019_3.geometry}
+							material={nodes.Cube019_3.material}
+							skeleton={nodes.Cube019_3.skeleton}
+							material-color="black"
+						/>
+						<skinnedMesh
+							name="Cube019_4"
+							geometry={nodes.Cube019_4.geometry}
+							material={nodes.Cube019_4.material}
+							skeleton={nodes.Cube019_4.skeleton}
+							material-color="yellow"
+						/>
+						<skinnedMesh
+							name="Cube019_5"
+							geometry={nodes.Cube019_5.geometry}
+							material={nodes.Cube019_5.material}
+							skeleton={nodes.Cube019_5.skeleton}
+							material-color="orange"
+						/>
+						<skinnedMesh
+							name="Cube019_6"
+							geometry={nodes.Cube019_6.geometry}
+							material={nodes.Cube019_6.material}
+							skeleton={nodes.Cube019_6.skeleton}
+							material-color="white"
+						/>
+						<skinnedMesh
+							name="Cube019_7"
+							geometry={nodes.Cube019_7.geometry}
+							material={nodes.Cube019_7.material}
+							skeleton={nodes.Cube019_7.skeleton}
+							material-color="purple"
+						/>
+						<skinnedMesh
+							name="Cube019_8"
+							geometry={nodes.Cube019_8.geometry}
+							material={nodes.Cube019_8.material}
+							skeleton={nodes.Cube019_8.skeleton}
+							material-color="pink"
+						/>
+						<skinnedMesh
+							name="Cube019_9"
+							geometry={nodes.Cube019_9.geometry}
+							material={nodes.Cube019_9.material}
+							skeleton={nodes.Cube019_9.skeleton}
+							material-color="cyan"
+						/>
+					</group>
+				</group>
+			</group>
+		</group>
 	);
 }
