a8b7018d18cd0b89ce9391ae676bf80cd09d02dc
William <william1504@gmail.com>
2022-09-23 13:10:42 -0300

    sem orbit sem like

--- src/components/atoms/HeroCanvas.tsx
+++ src/components/atoms/HeroCanvas.tsx
@@ -27,7 +27,7 @@ export default function HeroCanvas({ modelPath }: HeroCanvasProps) {
 				/>
 			</PerspectiveCamera>
 
-			<OrbitControls
+			{/* <OrbitControls
 				autoRotate
 				autoRotateSpeed={0.5}
 				rotateSpeed={0.5}
@@ -35,7 +35,7 @@ export default function HeroCanvas({ modelPath }: HeroCanvasProps) {
 				enableZoom={false}
 				maxPolarAngle={Math.PI / 2}
 				minPolarAngle={Math.PI / 2}
-			/>
+			/> */}
 
 			<Suspense fallback={null}>
 				<HeroModel
