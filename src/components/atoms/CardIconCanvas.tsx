b0d5293d1c90a62c44647c5daef5c5b611dafe35
Danilo Nobre <danilonobren@gmail.com>
2022-09-24 17:06:24 -0300

    color names, cards

--- src/components/atoms/CardIconCanvas.tsx
+++ src/components/atoms/CardIconCanvas.tsx
@@ -1,25 +1,25 @@
-import { Suspense, useRef } from 'react';
-import { Canvas } from '@react-three/fiber';
-import { PerspectiveCamera } from '@react-three/drei';
-import { useInView } from 'framer-motion';
+// import { Suspense, useRef } from 'react';
+// import { Canvas } from '@react-three/fiber';
+// import { PerspectiveCamera } from '@react-three/drei';
+// import { useInView } from 'framer-motion';
 
-import CardIconModel from '@atoms/CardIconModel';
+// import CardIconModel from '@atoms/CardIconModel';
 
-export interface CardIconCanvasProps {
-	fbxPath: string;
-}
+// export interface CardIconCanvasProps {
+// 	fbxPath: string;
+// }
 
-export default function CardIconCanvas({ fbxPath }: CardIconCanvasProps) {
-	const ref = useRef(null);
-	const isInView = useInView(ref, { once: true, amount: 1 });
+// export default function CardIconCanvas({ fbxPath }: CardIconCanvasProps) {
+// 	const ref = useRef(null);
+// 	const isInView = useInView(ref, { once: true, amount: 1 });
 
-	return (
-		<Canvas ref={ref}>
-			<PerspectiveCamera makeDefault fov={60} position={[0, 0, 10]} rotation={[0, 0, 0]} />
-			<pointLight position={[10, 10, 10]} intensity={1.2} />
-			<Suspense fallback={null}>
-				<CardIconModel modelPath={fbxPath} isInView={isInView} />
-			</Suspense>
-		</Canvas>
-	);
-}
+// 	return (
+// 		<Canvas ref={ref}>
+// 			<PerspectiveCamera makeDefault fov={60} position={[0, 0, 10]} rotation={[0, 0, 0]} />
+// 			<pointLight position={[10, 10, 10]} intensity={1.2} />
+// 			<Suspense fallback={null}>
+// 				<CardIconModel modelPath={fbxPath} isInView={isInView} />
+// 			</Suspense>
+// 		</Canvas>
+// 	);
+// }
--- src/components/atoms/ToggleCard.tsx
+++ src/components/atoms/ToggleCard.tsx
@@ -23,12 +23,12 @@ export default function ToggleCard({
 	const [isOpen, setOpen] = useState(false);
 
 	return (
-		<div className="card flex-grow: 1 w-96 min-h-[12rem] relative overflow-visible spacewiz__bg spacewiz__text--reversed">
-			<div className="absolute -mt-12 -mr-12 right-0 top-0 w-[150px] h-[150px] ">{cardIcon}</div>
-			<div className="card-body">
+		<div className="card flex-grow: 1 w-96 min-h-[12rem] relative overflow-visible shadow-2xl bg-gradient-to-b from-sw-secondary-200 to-sw-secondary-700 dark:from-sw-primary-600 dark:to-sw-primary-700  text-sw-secondary-500">
+			<div className="absolute -mt-12 -mr-12 right-0 top-0 w-[6rem] h-[6rem] bg-red-500">{cardIcon}</div>
+			<div className="card-body relative">
 				<>{!isOpen ? title : null}</>
 				<>{isOpen ? description : subtitle}</>
-				<div>
+				<div className="flex justify-end absolute -mr-2 -mb-2 right-0 bottom-0">
 					<motion.button
 						animate={{
 							rotate: isOpen ? 360 : 0,
@@ -37,7 +37,7 @@ export default function ToggleCard({
 						whileHover={{ scale: 1.1 }}
 						whileTap={{ scale: 0.9 }}
 						type="button"
-						className="rounded-full text-sm p-2.5 items-center spacewiz__text spacewiz__bg--reversed"
+						className="rounded-2xl text-sm p-2.5 items-center font-bold bg-sw-navy dark:bg-sw-flamingo text-sw-primary"
 						aria-label="Toggle"
 						onClick={() => setOpen(!isOpen)}
 					>
