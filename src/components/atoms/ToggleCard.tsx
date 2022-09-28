8af7e59289ad0324cc9314e193817cd1ca583faa
Danilo Nobre <danilonobren@gmail.com>
2022-09-28 02:38:08 -0300

    product grid, team grid, contact, footer

--- src/components/atoms/ToggleCard.tsx
+++ src/components/atoms/ToggleCard.tsx
@@ -23,8 +23,8 @@ export default function ToggleCard({
 	const [isOpen, setOpen] = useState(false);
 
 	return (
-		<div className="card w-[70%] lg:w-[25%] min-h-[12rem] relative overflow-visible shadow-2xl bg-gradient-to-b from-sw-secondary-200 to-sw-secondary-700 dark:from-sw-primary-600 dark:to-sw-primary-700  text-sw-secondary-500">
-			<div className="absolute -mt-4 -mr-12 right-0 top-0 w-16 h-16 md:w-24 md:h-24 bg-red-500">{cardIcon}</div>
+		<div className="card min-h-[12rem] relative overflow-visible shadow-lg bg-gradient-to-b from-sw-secondary-300 to-sw-secondary-600 dark:from-sw-primary-600 dark:to-sw-primary-800  text-sw-secondary-500">
+			<div className="absolute mx-auto -mt-8 md:-mt-4 md:-mr-12 right-0 left-0 md:left-auto top-0 w-16 h-16 md:w-24 md:h-24">{cardIcon}</div>
 			<div className="card-body relative">
 				<>{!isOpen ? title : null}</>
 				<>{isOpen ? description : subtitle}</>
@@ -37,7 +37,7 @@ export default function ToggleCard({
 						whileHover={{ scale: 1.1 }}
 						whileTap={{ scale: 0.9 }}
 						type="button"
-						className="rounded-xl text-sm p-2.5 items-center font-bold spacewiz__bg--accent text-sw-primary"
+						className="rounded-full text-sm p-2.5 items-center font-bold spacewiz__bg--accent text-sw-primary"
 						aria-label="Toggle"
 						onClick={() => setOpen(!isOpen)}
 					>
