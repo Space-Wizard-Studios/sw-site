b4269888c8b96ea62133e55dcf435e094529d41a
William <william1504@gmail.com>
2022-11-10 11:43:56 -0300

    socials animation

--- src/components/atoms/TeamCard/MemberSocials.tsx
+++ src/components/atoms/TeamCard/MemberSocials.tsx
@@ -1,30 +1,53 @@
+import { motion, LayoutGroup, AnimatePresence } from 'framer-motion';
 import { socials, Socials } from '@core/SocialLinks';
 import { Toggle } from './Toggle';
 
 interface Props {
 	links: Socials;
 	isOpen: boolean;
-	toggleOpen: Function;
+	toggleOpen: React.MouseEventHandler<HTMLButtonElement>;
 }
 
 export function MemberSocials({ links, isOpen, toggleOpen }: Props) {
 	return (
-		<div className="card-actions justify-end">
-			<div className="dropdown dropdown-top">
-				<Toggle onClick={toggleOpen} />
+		<LayoutGroup>
+			<motion.div
+				layout
+				style={{ borderRadius: '99999px' }}
+				transition={{
+					layout: {
+						staggerChildren: 1,
+					},
+				}}
+				className="card-actions justify-end bg-sw-primary-900"
+			>
+				<div className="flex p-4 gap-2">
+					<Toggle isOpen={isOpen} onClick={toggleOpen} />
 
-				{/* <ul className="dropdown-content menu p-2 shadow-md bg-base-100 rounded-box w-52 bg-sw-primary">
-					{Object.entries(links).map(([key, value]) => {
-						return (
-							<li key={key}>
-								<a target="_blank" href={value}>
-									{socials[key].name}
-								</a>
-							</li>
-						);
-					})}
-				</ul> */}
-			</div>
-		</div>
+					<AnimatePresence>
+						{isOpen &&
+							Object.entries(links).map(([key, value]) => {
+								return (
+									<motion.a
+										key={key}
+										target="_blank"
+										href={value}
+										initial={{ opacity: 0, translateX: '-100%' }}
+										animate={{ opacity: 1, translateX: '0%', transition: { delay: 0.2 } }}
+										exit={{ opacity: 0, translateX: '-100%', transition: { duration: 0.2 } }}
+										whileHover={{ scale: 1.1 }}
+										whileTap={{ scale: 0.9 }}
+										className="relative z-0 w-10 h-10 rounded-full bg-sw-navy dark:bg-sw-flamingo text-sw-primary border-none"
+									>
+										{socials[key].icon({
+											className: 'absolute m-auto top-0 bottom-0 left-0 right-0 w-8 h-8',
+										})}
+									</motion.a>
+								);
+							})}
+					</AnimatePresence>
+				</div>
+			</motion.div>
+		</LayoutGroup>
 	);
 }
--- src/components/atoms/TeamCard/Toggle/index.tsx
+++ src/components/atoms/TeamCard/Toggle/index.tsx
@@ -1,18 +1,21 @@
 import { motion } from 'framer-motion';
+import { Minus, Plus } from '@icons/card_button';
 
 interface Props {
-	onClick: Function;
+	isOpen: boolean;
+	onClick: React.MouseEventHandler<HTMLButtonElement>;
 }
 
-export function Toggle({ onClick }: Props) {
+export function Toggle({ isOpen, onClick }: Props) {
 	return (
 		<motion.button
+			layout
 			whileHover={{ scale: 1.1 }}
 			whileTap={{ scale: 0.9 }}
-			onClick={() => onClick()}
-			className=" w-12 h-12 rounded-full bg-sw-navy dark:bg-sw-flamingo text-sw-primary border-none"
+			onClick={(e) => onClick(e)}
+			className="z-10 w-10 h-10 p-3 rounded-full bg-sw-navy dark:bg-sw-flamingo text-sw-primary border-none"
 		>
-			+
+			{isOpen ? <Minus className="m-auto w-4 h-4" /> : <Plus className="m-auto w-4 h-4" />}
 		</motion.button>
 	);
 }
