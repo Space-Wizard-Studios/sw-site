357e26d17ab909af990195478560b46474a8312e
William Magalhães <william1504@gmail.com>
2023-01-04 14:14:55 -0300

    Project Pages (#13)
    * project pages
    
    * rm astro-imagetools
    
    * erro 404
    
    * hero minor fix, implementação inicial projeto
    
    * small fixes
    
    * ToggleTheme useLayoutEffect
    
    * remove </path>
    
    * @widgets
    
    * newline at end of file
    
    * 404 to home button
    
    * json não pode ter virgula no fim da array
    
    * deploy on edit pull request
    
    * edit?
    
    * update firebase-tools
    
    * npm update
    
    * pull_request:
    
    * minor fixes, project layout
    
    * erro de digitação
    
    * project hero props
    
    * pass props to components
    
    * partners carousel & details props
    
    * mb rounded-b
    
    * project layout
    
    * project page
    
    * teste de build com o /* @vite-ignore */
    
    * remove other pages
    
    * carousel do caralho
    
    * project page
    
    * ImageProps import
    
    * base projects content
    
    * projects on folders
    
    * props
    
    * sem export nos Props normais
    
    * jornada_1
    
    * stylelint
    
    * tailwind config
    
    * readme codacy url
    
    * - override
    
    Co-authored-by: Danilo Nobre Nunes <danilonobren@gmail.com>

--- src/components/atoms/ProductCard/ProductTitle.tsx
+++ src/components/atoms/ProductCard/ProductTitle.tsx
@@ -6,8 +6,8 @@ interface Props {
 export function ProductTitle({ title, subtitle }: Props) {
 	return (
 		<div className="grid grid-flow-row grid-rows-2 items-start">
-			<h2 className="spacewiz__text--accent">{title}</h2>
-			<h3 className="spacewiz__text">{subtitle}</h3>
+			<h3 className="spacewiz__text--accent">{title}</h3>
+			<h4 className="spacewiz__text">{subtitle}</h4>
 		</div>
 	);
 }
