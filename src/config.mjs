fbdfd72c6046e2b2ca260c544e9810d08010e730
Danilo Nobre <danilonobren@gmail.com>
2023-01-09 17:24:01 -0300

    23-metatags (#30)
    * merge dev to main, issue #6 (#29)
    
    * Fix da Animação no Card de TeamMember (#22)
    
    - Container sempre tem o tamanho correto
    - Transição corrigida
    
    * closes #3 (#24)
    
    * sitemap fix
    
    * merge seção contato (#28)
    
    * implementação inicial
    
    * padronização de títulos da home
    
    * margin
    
    * closes #6
    
    Co-authored-by: William Magalhães <william1504@gmail.com>
    
    * closes #23
    
    Co-authored-by: William Magalhães <william1504@gmail.com>

--- src/config.mjs
+++ src/config.mjs
@@ -1,11 +1,17 @@
 export const SITE = {
 	name: 'spacewiz.dev',
 
-  origin: 'https://spacewiz.dev',
-  basePathname: '/',
+	origin: 'https://spacewiz.dev',
+	basePathname: '/',
+	// trailingSlash: false,
 
 	title: 'Space Wizard Studios',
 	description: 'Da direção à criação, buscamos sempre inovar com o melhor da produção criativa: trazemos o seus projetos de apps, jogos e páginas à vida! 🚀',
+
+	defaultImage: '@images/meta.png',
+
+	language: 'pt-BR',
+	textDirection: 'ltr',
 
 	googleAnalyticsId: '337095792',
 	googleSiteVerificationId: 'psQe6AIBb-tp74gfg1VJcAbH2EseZF8LNVwsGJCyQRI',
