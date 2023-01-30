c782869b593da387879aa9b1b5069ae35d458a89
Danilo Nobre Nunes <danilonobren@gmail.com>
2023-01-30 00:11:57 -0300

    as 'ul'

--- src/components/atoms/MenuLinks.tsx
+++ src/components/atoms/MenuLinks.tsx
@@ -28,54 +28,50 @@ export function MenuLinks() {
 							<path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd"></path>
 						</svg>
 					</Menu.Button>
-					<ul className="absolute p-2 m-4 z-10 grid grid-cols-2 text-xs bg-white border rounded-lg shadow-md">
-						<Menu.Items as={Fragment} >
-							{projects.map((link) => (
-								<Menu.Item key={link.href} as={Fragment}>
-									{({ active }) => (
-										<li className="m-2">
-											<a
-												href={link.href}
-												className={`${active ? 'text-gray-500 dark:text-gray-400 hover:text-blue-600 dark:hover:text-blue-500' : 'bg-white text-black'
-													}`}
-											>
-												{link.label}
-											</a>
-										</li>
-									)}
-								</Menu.Item>
-							))}
-						</Menu.Items>
-					</ul>
+					<Menu.Items as={'ul'} className="absolute p-2 mt-4 z-10 grid text-xs bg-white border rounded-lg shadow-md">
+						{projects.map((link) => (
+							<Menu.Item key={link.href} as={Fragment}>
+								{({ active }) => (
+									<li className="m-2">
+										<a
+											href={link.href}
+											className={`${active ? 'text-gray-500 dark:text-gray-400 hover:text-blue-600 dark:hover:text-blue-500' : 'bg-white text-black'
+												}`}
+										>
+											{link.label}
+										</a>
+									</li>
+								)}
+							</Menu.Item>
+						))}
+					</Menu.Items>
 				</Menu>
 			</li>
 			<li>
-				<Menu>
+				<Menu >
 					<Menu.Button className="flex items-center justify-between w-full">
 						<span>Suporte</span>
 						<svg aria-hidden="true" className="w-5 h-5 ml-1 md:w-4 md:h-4" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
 							<path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd"></path>
 						</svg>
 					</Menu.Button>
-					<ul className="absolute p-2 mt-4 z-10 grid text-xs bg-white border rounded-lg shadow-md">
-						<Menu.Items as={Fragment}>
-							{support.map((link) => (
-								<Menu.Item key={link.href} as={Fragment}>
-									{({ active }) => (
-										<li className="m-2">
-											<a
-												href={link.href}
-												className={`${active ? 'text-gray-500 dark:text-gray-400 hover:text-blue-600 dark:hover:text-blue-500' : 'bg-white text-black'
-													}`}
-											>
-												{link.label}
-											</a>
-										</li>
-									)}
-								</Menu.Item>
-							))}
-						</Menu.Items>
-					</ul>
+					<Menu.Items as={'ul'} className="absolute p-2 mt-4 z-10 grid text-xs bg-white border rounded-lg shadow-md">
+						{support.map((link) => (
+							<Menu.Item key={link.href} as={Fragment}>
+								{({ active }) => (
+									<li className="m-2">
+										<a
+											href={link.href}
+											className={`${active ? 'text-gray-500 dark:text-gray-400 hover:text-blue-600 dark:hover:text-blue-500' : 'bg-white text-black'
+												}`}
+										>
+											{link.label}
+										</a>
+									</li>
+								)}
+							</Menu.Item>
+						))}
+					</Menu.Items>
 				</Menu>
 			</li>
 
