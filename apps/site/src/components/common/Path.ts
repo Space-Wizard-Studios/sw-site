70a6259e969c6cf25da3bc6287f3ff585df84d57
Danilo Nobre <danilonobren@gmail.com>
2025-04-12 02:09:30 -0300

    refactor: remove unused components and scripts for cleaner codebase

--- apps/site/src/components/common/Path.ts
+++
@@ -1,7 +0,0 @@
-export function getSlugFromURL(url?: string) {
-	return url?.split('/').reverse()[0].split('.')[0];
-}
-
-export function getLocaleFromURL(url?: string) {
-	return url?.split('/')[2];
-}
