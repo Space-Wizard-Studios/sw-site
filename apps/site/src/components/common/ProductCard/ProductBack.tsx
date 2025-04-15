83159272107e1bbecf30579dd8f9616bfa8aed2f
Danilo Nobre <danilonobren@gmail.com>
2025-04-15 20:17:34 -0300

    feat: update theme colors, enhance About section, and restructure Products component for improved layout and readability

--- apps/site/src/components/common/ProductCard/ProductBack.tsx
+++ apps/site/src/components/common/ProductCard/ProductBack.tsx
@@ -13,12 +13,10 @@ export function ProductBack({ isOpen, description }: Props) {
     return (
         <div
             className={cn(
-                'absolute left-0 top-0 h-full w-full text-sm',
-                'bg-radial-[at_15%_15%]',
+                'absolute bottom-0 right-0 h-full w-full text-sm',
+                'bg-linear-30',
                 'text-on-surface from-surface-container-high to-surface-container-highest',
-                'transition-all duration-300 ease-in-out',
-                isOpen ? 'z-10' : 'z-0',
-                isOpen ? 'opacity-100' : 'opacity-0',
+                'transition-all duration-1000 ease-in-out',
                 isOpen ? 'pointer-events-auto' : 'pointer-events-none',
                 isOpen ? '' : 'hidden',
             )}
