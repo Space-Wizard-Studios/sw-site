e9478015de829625895abdda918e0d5e0f58c096
Danilo Nobre <danilonobren@gmail.com>
2025-04-28 00:48:48 -0300

    fix: update styles for links and lists in Footer and Header components; adjust import path for PrivacyContent

--- apps/site/src/components/common/SiteSocials/index.tsx
+++ apps/site/src/components/common/SiteSocials/index.tsx
@@ -3,7 +3,7 @@ import { SocialLink } from './SocialLink';
 
 export function SiteSocials() {
     return (
-        <ul className='flex list-none flex-row flex-wrap gap-4'>
+        <ul className='flex list-none flex-row flex-wrap gap-4 pl-0'>
             {Object.entries(siteSocials).map(([key, { url, aria }]) => (
                 <li key={key}>
                     <SocialLink socialKey={key} url={url} aria={aria} />
